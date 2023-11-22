3a5438d87dfc294347dcce0043866d338e70b1ec
Justin White <kyjus25@gmail.com>
2023-11-21 19:02:04 -0600

    Updates

--- gen/gen.js
+++ gen/gen.js
@@ -1,34 +1,46 @@
 "use strict";
 const fs = require("fs");
 const apidoc = require("./apidoc.json");
+const BLACKLIST = [
+    'AccordionTab',
+    'CSSTransition'
+];
 // Create directory
 fs.rmSync("./stories", { recursive: true, force: true });
 fs.mkdirSync("./stories");
 // console.log(apidoc);
-const groups = Object.keys(apidoc).filter((componentName) => componentName.indexOf("/") === -1);
-const components = groups
+const importPaths = Object.keys(apidoc).filter((componentName) => componentName.indexOf("/") === -1 && componentName.indexOf("-") === -1);
+const components = importPaths
     .flatMap((group) => apidoc[group].components)
-    .filter((component) => !!component);
+    .filter((component) => !!component)
+    .flatMap(components => {
+    return Object.keys(components).map(name => (Object.assign(Object.assign({}, components[name]), { name })));
+})
+    .filter(component => BLACKLIST.indexOf(component.name) === -1);
 console.log(components);
 // Add file
-const file = `
-  import { Button } from 'primereact/button';
+components.forEach((component) => {
+    const file = `
+    import { ${component.name} } from 'primereact/${component.name.toLowerCase()}';
 
-  export default {
-    title: 'Example/Button',
-    component: Button,
-    parameters: {
-      layout: 'centered',
-    },
-    tags: ['autodocs'],
-    argTypes: {
-      backgroundColor: { control: 'color' },
-    },
-  };
+    export default {
+      title: '${component.name}',
+      component: ${component.name},
+      parameters: {
+        layout: 'centered',
+      },
+      argTypes: {
+        backgroundColor: { control: 'color' },
+      },
+    };
 
-
-`;
-groups.forEach((componentName) => {
-    fs.writeFileSync(`./stories/${componentName}.stories.js`, file);
+    export const Primary = {
+      args: {
+        primary: true,
+        label: 'Button',
+      },
+    };
+  `;
+    fs.writeFileSync(`./stories/${component.name}.stories.js`, file);
 });
 setTimeout(() => { }, 100000000);
--- gen/gen.ts
+++ gen/gen.ts
@@ -1,43 +1,76 @@
 const fs = require("fs");
 const apidoc = require("./apidoc.json");
 
+const BLACKLIST: string[] = [
+  'AccordionTab',
+  'CSSTransition'
+]
+
+interface Component {
+  name: string,
+  description: string,
+  methods: {
+    description: string,
+    values: any[]
+  },
+  props: {
+    description: string,
+    values: any[]
+  },
+  callbacks: {
+    description: string,
+    values: any[]
+  }
+}
+
 // Create directory
 fs.rmSync("./stories", { recursive: true, force: true });
 fs.mkdirSync("./stories");
 
 // console.log(apidoc);
-const groups = Object.keys(apidoc).filter(
-  (componentName) => componentName.indexOf("/") === -1
+const importPaths = Object.keys(apidoc).filter(
+  (componentName) => componentName.indexOf("/") === -1 && componentName.indexOf("-") === -1
 );
 
-const components = groups
+const components: Component[] = importPaths
   .flatMap((group) => apidoc[group].components)
-  .filter((component) => !!component);
+  .filter((component) => !!component)
+  .flatMap(components => {
+    return Object.keys(components).map(name => ({
+      ...components[name],
+      name
+    }))
+  })
+  .filter(component => BLACKLIST.indexOf(component.name) === -1);
+
 
 console.log(components);
 
 // Add file
 
-const file = `
-  import { Button } from 'primereact/button';
+components.forEach((component) => {
+  const file = `
+    import { ${component.name} } from 'primereact/${component.name.toLowerCase()}';
 
-  export default {
-    title: 'Example/Button',
-    component: Button,
-    parameters: {
-      layout: 'centered',
-    },
-    tags: ['autodocs'],
-    argTypes: {
-      backgroundColor: { control: 'color' },
-    },
-  };
+    export default {
+      title: '${component.name}',
+      component: ${component.name},
+      parameters: {
+        layout: 'centered',
+      },
+      argTypes: {
+        backgroundColor: { control: 'color' },
+      },
+    };
 
-
-`;
-
-groups.forEach((componentName) => {
-  fs.writeFileSync(`./stories/${componentName}.stories.js`, file);
+    export const Primary = {
+      args: {
+        primary: true,
+        label: 'Button',
+      },
+    };
+  `;
+  fs.writeFileSync(`./stories/${component.name}.stories.js`, file);
 });
 
 setTimeout(() => {}, 100000000);
