d9cfeb162da3a31ca89f126acb8910cc6282f0f5
Justin White <kyjus25@gmail.com>
2023-11-22 12:32:16 -0600

    Loading in styles

---
+++ .storybook/preview.jsx
@@ -0,0 +1,31 @@
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeicons/primeicons.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  decorators: [
+    (Story) => (
+      <PrimeReactProvider>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </PrimeReactProvider>
+    ),
+  ],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    options: {
+      storySort: {
+        order: ['Form', 'Button', ['Home', 'Login', 'Admin'], 'Components'],
+      },
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
--- gen/gen.ts
+++ gen/gen.ts
@@ -2,67 +2,115 @@ const fs = require("fs");
 const apidocJSON = require("./apidoc.json");
 const menuJSON = require("./menu.json");
 
-const BLACKLIST: string[] = [
-  'AccordionTab',
-  'CSSTransition'
-]
+const BLACKLIST: string[] = ["AccordionTab", "CSSTransition"];
+
+interface Prop {
+  default: string;
+  description: string;
+  name: string;
+  optional: boolean;
+  readonly: boolean;
+  type: string;
+}
 
 interface Component {
-  name: string,
-  description: string,
-  category: string,
+  name: string;
+  description: string;
+  category: string;
   methods: {
-    description: string,
-    values: any[]
-  },
+    description: string;
+    values: any[];
+  };
   props: {
-    description: string,
-    values: any[]
-  },
+    description: string;
+    values: Prop[];
+  };
   callbacks: {
-    description: string,
-    values: any[]
-  }
+    description: string;
+    values: any[];
+  };
 }
 
 // Create directory
 fs.rmSync("./stories", { recursive: true, force: true });
 fs.mkdirSync("./stories");
 
-const menu = menuJSON.data.find((i: any) => i.name === 'Components').children;
+const menu = menuJSON.data.find((i: any) => i.name === "Components").children;
 
-console.log('menu', menu);
+console.log("menu", menu);
 
 // console.log(apidoc);
 const importPaths = Object.keys(apidocJSON).filter(
-  (componentName) => componentName.indexOf("/") === -1 && componentName.indexOf("-") === -1
+  (componentName) =>
+    componentName.indexOf("/") === -1 && componentName.indexOf("-") === -1
 );
+
+const icons = Object.keys(apidocJSON)
+  .filter((componentName) => componentName.indexOf("icons/") !== -1)
+  .map((icon) => `pi pi-${icon.replace("icons/", "")}`);
+
+console.log(icons);
+
+const translateProp = (
+  prop: Prop
+): { control: string; description: string; options: string[] | null } => {
+  const description = prop.description;
+  let type = "object";
+  let options = null;
+  if (prop.type === "string") type = "text";
+  if (prop.type === "boolean") type = "boolean";
+  if (prop.type === "number") type = "number";
+  if (
+    prop.type.indexOf("ReactNode") !== -1 ||
+    prop.type.indexOf("ReactElement") !== -1
+  ) {
+    type = "text";
+  } else if (prop.type.indexOf(" | ") !== -1) {
+    options = prop.type.replace(/\"/g, "").split(" | ");
+    type = "select";
+  }
+  if (prop.type.indexOf("Icon") !== -1) {
+    options = icons;
+    type = "select";
+  }
+  if (type === "object") {
+    console.log("need to define", prop.name, prop.type);
+  }
+  return { control: type, description, options };
+};
 
 const components: Component[] = importPaths
   .flatMap((group) => apidocJSON[group].components)
   .filter((component) => !!component)
-  .flatMap(components => {
-    return Object.keys(components).map(name => ({
+  .flatMap((components) => {
+    return Object.keys(components).map((name) => ({
       ...components[name],
-      category: menu.find((category: any) => 
+      category: menu.find((category: any) =>
         category.children.find((comp: any) => comp.name === name)
-      )?.name, 
-      name
-    }))
+      )?.name,
+      name,
+    }));
   })
-  .filter(component => !!component.category)
-  .filter(component => BLACKLIST.indexOf(component.name) === -1);
-
+  .filter((component) => !!component.category)
+  .filter((component) => BLACKLIST.indexOf(component.name) === -1);
 
 console.log(components);
-
-const args = { backgroundColor: { control: 'color' } }
 
 // Add file
 
 components.forEach((component) => {
+  const argTypes = component.props.values.reduce(
+    (obj, prop) => ({
+      ...obj,
+      [prop.name]: translateProp(prop),
+    }),
+    {}
+  );
+
   const file = `
-    import { ${component.name} } from 'primereact/${component.name.toLowerCase()}';
+    import { ${
+      component.name
+    } } from 'primereact/${component.name.toLowerCase()}';
 
     export default {
       title: '${component.category}/${component.name}',
@@ -70,7 +118,7 @@ components.forEach((component) => {
       parameters: {
         layout: 'centered',
       },
-      argTypes: ${JSON.stringify(args)},
+      argTypes: ${JSON.stringify(argTypes)},
     };
 
     export const Default = {
@@ -81,7 +129,7 @@ components.forEach((component) => {
     };
     
   `;
-  fs.writeFileSync(`./stories/${component.name}.stories.js`, file);
+  fs.writeFileSync(`./stories/${component.name}.stories.jsx`, file);
 });
 
 setTimeout(() => {}, 100000000);
